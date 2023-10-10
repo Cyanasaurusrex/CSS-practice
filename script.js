69ced35d7d3466581f3da190f83452990253569f
Nathan Moon <moonman700@gmail.com>
2023-10-10 00:29:46 -0400

    Added degree counter

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const wheel = document.getElementById('wheel');
 const spinButton = document.getElementById('spinButton');
+const resetButton = document.getElementById('resetButton');
 
 let currentRotation = 0; // Track the current rotation of the wheel
 const minRotation = 720; // Minimum degrees to rotate
 const maxRotation = 1440; // Maximum degrees to rotate
-let spinTime = 1000; // spin time in ms
+let totalRotation = 0
+let spinTime = 10000; // spin time in ms
+let logTime = 100 // log frequency in ms
+let seconds = 0
+
 
 function logHello() {
-    console.log('hello');
+    seconds += logTime/1000
+    console.log("Current degrees: " +  Math.floor(((totalRotation - currentRotation) / (spinTime / 1000)) * seconds + 0.5))
 }
 
 function spinWheel() {
@@ -16,7 +22,7 @@ function spinWheel() {
     
 
     // Calculate the total rotation including the current rotation
-    const totalRotation = currentRotation + randomDegree;
+    totalRotation = currentRotation + randomDegree;
 
     console.log("moving from " + currentRotation + " to " + totalRotation)
 
@@ -24,20 +30,27 @@ function spinWheel() {
     wheel.style.transition = `transform ${spinTime / 1000}s ease-out`;
     wheel.style.transform = `rotate(${totalRotation}deg)`;
 
-    // Update the current rotation
-    currentRotation = totalRotation;    
+    intervalId = setInterval(function () {
+        logHello()
+    }, logTime); // Log 'hello' every second
+
+       
 
     // Disable the button temporarily to prevent multiple spins
-    spinButton.disabled = true;
-
-    intervalId = setInterval(logHello, 1000); // Log 'hello' every second
+    spinButton.disabled = true;    
 
     // Reset the wheel after the animation
     setTimeout(() => {
         wheel.style.transition = 'none';
         spinButton.disabled = false;
         clearInterval(intervalId)
+        currentRotation = totalRotation; 
     }, spinTime);
 }
 
+function resetWheel() {
+
+}
+
 spinButton.addEventListener('click', spinWheel);
+resetButton.addEventListener('click', resetWheel);
